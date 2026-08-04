440d13c04d71d66336d5e9d9d8943c0c56baffb9
PIYUSH KUMAR MISHRA <155824301+PIYUSHMISHRA1009@users.noreply.github.com>
2026-08-04 16:12:34 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.length();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        int start=0;
+        int maxLen=1;
+        string res="";
+        for(int i=0;i<s.length();i++){
+            dp[i][i]=true;
+        }
+        for(int len=2;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] && (len<=3 || dp[i+1][j-1])){
+                    dp[i][j]=true;
+                    if(len>maxLen){
+                        maxLen=len;
+                        start=i;
+                    }
+                }
+            }
+        }
+        return s.substr(start,maxLen);
+    }
+};
