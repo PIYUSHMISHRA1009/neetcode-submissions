d66fcdf115131d55f826cfc0a149d796c7bcec80
PIYUSH KUMAR MISHRA <155824301+PIYUSHMISHRA1009@users.noreply.github.com>
2026-08-05 19:03:28 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,47 @@
+class MinStack {
+    //memorize this soln please
+public:
+    stack<int> st;
+    int minelement=-1;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(val);
+            minelement=val;
+        }
+        else if(val<minelement){
+            st.push(2*val-minelement);
+            minelement=val;
+        }
+        else{
+            st.push(val);
+        }
+    }
+    
+    void pop() {
+        if(st.top()>=minelement){
+            st.pop();
+        }
+        else{
+            int prevmin=2*minelement-st.top();
+            minelement=prevmin;
+            st.pop();
+        }
+    }
+    
+    int top() {
+        if(st.top()<minelement){
+            return minelement;
+        }
+        else{
+            return st.top();
+        }
+    }
+    
+    int getMin() {
+        return minelement;
+    }
+};
