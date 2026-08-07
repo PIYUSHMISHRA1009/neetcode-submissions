0ba8946d0a07004226b7787205e884d60e2d1566
PIYUSH KUMAR MISHRA <155824301+PIYUSHMISHRA1009@users.noreply.github.com>
2026-08-07 11:18:01 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+public:
+    unordered_map<string,vector<pair<int,string>>> mpp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mpp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if(mpp.find(key) == mpp.end()) return "";
+        string res="";
+        auto &it=mpp[key];
+        int low=0;
+        int high=it.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(it[mid].first<=timestamp){
+                res=it[mid].second;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return res;
+    }
+};
